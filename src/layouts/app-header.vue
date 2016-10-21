41e57da5337a20c6caa40591672b6a7a08f6c52c
chenke91 <chenke91@qq.com>
2016-10-21 20:56:10 +0800

    add userInfo

--- src/layouts/app-header.vue
+++ src/layouts/app-header.vue
@@ -13,7 +13,7 @@
       <ul class="nav navbar-nav nav-pills navbar-right">
         <li class="dropdown">
           <a href="#" class="user-dropdown" data-toggle="dropdown">
-            {{user.realName}} <span class="caret"></span>
+            {{user.name}} <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
             <li>

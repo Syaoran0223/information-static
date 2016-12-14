1c77ceb998816cdc0e0d44f267ea259dc525335b
chenke91 <chenke91@qq.com>
2016-12-14 20:41:14 +0800

    题目录入

--- src/modules/paper-input/index.vue
+++ src/modules/paper-input/index.vue
@@ -38,12 +38,57 @@
             </div>
           </div>
 
+          <option-view
+              :value.sync="option_values">
+          </option-view>
+
           <div class="form-group">
-            <label for="" class="control-label col-sm-1">选项</label>
+            <label for="" class="control-label col-sm-1">答案</label>
             <div class="col-sm-11">
-              <option-view
-                  :value.sync="option_values">
-              </option-view>
+              <input-alert
+                  :value.sync="edit.formData.answer"
+                  placehold="点击此处输入答案">
+              </input-alert>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="" class="control-label col-sm-1">解答</label>
+            <div class="col-sm-11">
+              <input-alert
+                  :value.sync="edit.formData.jieda"
+                  placehold="点击此处输入解答">
+              </input-alert>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="" class="control-label col-sm-1">分析</label>
+            <div class="col-sm-11">
+              <input-alert
+                  :value.sync="edit.formData.fenxi"
+                  placehold="点击此处输入分析">
+              </input-alert>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="" class="control-label col-sm-1">点评</label>
+            <div class="col-sm-11">
+              <input-alert
+                  :value.sync="edit.formData.dianpin"
+                  placehold="点击此处输入点评">
+              </input-alert>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="" class="control-label col-sm-1">考点</label>
+            <div class="col-sm-11">
+              <input-alert
+                  :value.sync="edit.formData.kaodian"
+                  placehold="点击此处输入考点">
+              </input-alert>
             </div>
           </div>
 
@@ -82,7 +127,8 @@
           {
             sort: 'A',
             content: '',
-            _id: _.uniqueId('option_')
+            _id: _.uniqueId('option_'),
+            _selected: false
           }
         ]
       }
--- src/modules/paper-input/option-view.vue
+++ src/modules/paper-input/option-view.vue
@@ -1,34 +1,45 @@
 <template>
   <div>
-    <div class="mg-t-7" v-if="!value.length">
-      <button
-          class="btn btn-default btn-xs"
-          @click.prevent="insert()">
-        新增
-      </button>
-    </div>
-    <div v-for="item in value">
-      <div class="row">
-        <div class="col-sm-1">
-          <div class="mg-t-7">{{item.sort}}</div>
-        </div>
-        <div class="col-sm-5">
-          <input-alert
-              :value.sync="item.content"
-              placehold="点击此处输入选项">
-          </input-alert>
-        </div>
-        <div class="col-sm-2">
-          <button class="btn btn-default btn-xs"
-            @click.prevent="remove(item._id)">
-            删除
-          </button>
-          <button
-              v-if="$index + 1 == value.length"
-              class="btn btn-default btn-xs"
-              @click.prevent="insert()">
+    <div class="form-group">
+      <label for="" class="control-label col-sm-1">选项</label>
+      <div class="col-sm-11">
+        <div class="mg-t-7" v-if="!value.length">
+          <button class="btn btn-default btn-xs" @click.prevent="insert()">
             新增
           </button>
+        </div>
+        <div v-for="item in value" class="row">
+          <div class="inline">
+            <div class="mg-t-7">{{item.sort}}</div>
+          </div>
+          <div class="inline">
+            <input-alert :value.sync="item.content" placehold="点击此处输入选项">
+            </input-alert>
+          </div>
+          <div class="inline">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" v-model="item._selected"> 正确答案
+              </label>
+            </div>
+          </div>
+          <div class="inline" style="margin-top: 7px;">
+            <button class="btn btn-default btn-xs" @click.prevent="remove(item._id)">
+              删除
+            </button>
+            <button v-if="$index + 1 == value.length" class="btn btn-default btn-xs" @click.prevent="insert()">
+              新增
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="" class="control-label col-sm-1">答案</label>
+      <div class="col-sm-11">
+        <div class="mg-t-7">
+          <span v-for="item in selected">{{item.sort}}</span>
         </div>
       </div>
     </div>
@@ -48,6 +59,20 @@
     components: {
       InputAlert
     },
+    computed: {
+      selected() {
+        return _.filter(this.value, (item)=> {
+          return item._selected
+        })
+      }
+    },
+    watch: {
+      value(val) {
+        _.forEach(this.value, (item, key) => {
+          item.sort = String.fromCharCode(key+65)
+        })
+      }
+    },
     methods: {
       remove(_id) {
         let items = _.clone(this.value)
@@ -59,12 +84,19 @@
       insert() {
         this.value.push(
           {
-            sort: 'A',
+            sort: '',
             content: '',
-            _id: _.uniqueId('option_')
+            _id: _.uniqueId('option_'),
+            _selected: false
           }
         )
       }
     }
   }
 </script>
+<style>
+  .inline {
+    float: left !important;
+    margin: 0px 10px 0px 10px;
+  }
+</style>

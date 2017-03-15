4561478168e8d5e3b0833d0a43419fdaa4ed368a
chenke91 <chenke91@qq.com>
2017-03-15 21:33:10 +0800

    debug

--- src/modules/answer-list/table.vue
+++ src/modules/answer-list/table.vue
@@ -3,14 +3,14 @@
     :data="table"
     @page-index-change="page_index_change"
   >
-
     <div class="" v-for="item in table.items">
       <div class="flex-grid"  @click="item.open=!item.open">
-        <div class="grid-cell">{{item.exam.year}} {{item.exam.section | get_const_value 'section'}}</div>
-        <div class="grid-cell">{{item.exam.school_name}}</div>
-        <div class="grid-cell cell-label">{{item.exam.grade | get_const_value 'grade'}}</div>
-        <div class="grid-cell cell-label">{{item.exam.subject | get_const_value 'subject'}}</div>
-        <div class="grid-cell">{{item.exam.name}}</div>
+        <div class="grid-cell">{{item.year}} {{item.section | get_const_value 'section'}}</div>
+        <div class="grid-cell">{{item.school_name}}</div>
+        <div class="grid-cell cell-label">{{item.grade | get_const_value 'grade'}}</div>
+        <div class="grid-cell cell-label">{{item.subject | get_const_value 'subject'}}</div>
+        <div class="grid-cell">{{item.name}}</div>
+        <div class="grid-cell">上传时间: {{item.created_at | date_format}}</div>
         <div class="grid-cell cell-btn-group">
           <div class="grid-cell cell-label">
             <span class="glyphicon glyphicon-chevron-up" v-show="item.open"></span>
@@ -21,7 +21,7 @@
       <div class="panel panel-default" v-show="item.open">
         <div class="panel-body">
           <table class="table table-striped">
-            <tr v-for="data in item.items">
+            <tr v-for="data in item.questions">
               <td>第 {{data.quest_no}} 题</td>
               <td>{{data.question.quest_type_id | get_const_value 'quest_types'}}</td>
               <td>{{data.question.has_sub | get_const_value 'has_sub'}}</td>
--- src/modules/wait-judge/table.vue
+++ src/modules/wait-judge/table.vue
@@ -5,19 +5,38 @@
   >
 
     <div class="">
-      <div class="flex-grid" v-for="item in table.items">
-        <div class="grid-cell">{{item.exam_year}} {{item.exam_section | get_const_value 'section'}}</div>
-        <div class="grid-cell">{{item.school_name}}</div>
-        <div class="grid-cell cell-label">{{item.exam_grade | get_const_value 'grade'}}</div>
-        <div class="grid-cell cell-label">{{item.exam_subject | get_const_value 'subject'}}</div>
-        <div class="grid-cell">{{item.exam_name}}</div>
-        <div class="grid-cell">第 {{item.quest_no}} 题</div>
-        <div class="grid-cell cell-btn-group">
-          <button class="btn btn-link"
-              @click="on_item_edit_click(item)"
-          >
-            <span>我要裁定</span>
-          </button>
+      <div class="" v-for="item in table.items">
+        <div class="flex-grid" @click="item.open=!item.open">
+          <div class="grid-cell">{{item.year}} {{item.section | get_const_value 'section'}}</div>
+          <div class="grid-cell">{{item.school_name}}</div>
+          <div class="grid-cell cell-label">{{item.grade | get_const_value 'grade'}}</div>
+          <div class="grid-cell cell-label">{{item.subject | get_const_value 'subject'}}</div>
+          <div class="grid-cell">{{item.name}}</div>
+          <div class="grid-cell">上传时间: {{item.created_at | date_format}}</div>
+          <div class="grid-cell cell-btn-group">
+            <div class="grid-cell cell-label">
+              <span class="glyphicon glyphicon-chevron-up" v-show="item.open"></span>
+              <span class="glyphicon glyphicon-chevron-down" v-show="!item.open"></span>
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default" v-show="item.open">
+          <div class="panel-body">
+            <table class="table table-striped">
+              <tr v-for="data in item.questions">
+                <td>第 {{data.quest_no}} 题</td>
+                <td>{{data.quest_type_id | get_const_value 'quest_types'}}</td>
+                <td>{{data.has_sub | get_const_value 'has_sub'}}</td>
+                <td>{{data.state | get_const_value 'question_state'}}</td>
+                <td>
+                  <button class="btn btn-link"
+                    @click="on_item_edit_click(data)">
+                    <span>我要裁定</span>
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>

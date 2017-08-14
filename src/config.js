37db380b14e02444b7dc0a892d6917dfb7d370a3
chenke91 <chenke91@qq.com>
2017-08-14 19:37:16 +0800

    add subject data

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const api_host = window.siteUrl || 'http://127.0.0.1:5000'
 export const user = window.userInfo || {}
 export const access_menus = window.menus || []
+export const subjects = window.subjects || []

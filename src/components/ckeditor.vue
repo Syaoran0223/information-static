bbacf35b578113b953b1b40c9fe7a527a8a4b519
chenke91 <chenke91@qq.com>
2017-09-13 19:34:06 +0800

    debug

--- src/components/ckeditor.vue
+++ src/components/ckeditor.vue
@@ -19,22 +19,6 @@
     { name: 'document', items: [ 'Source' ] },
   ]
 
-  const toolbar_group = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
-	]
-
   export default {
     props: {
       value: {

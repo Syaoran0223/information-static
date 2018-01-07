628ddba08decbc886e2fafeb325069f2305545c7
chenke <chenke91@qq.com>
2018-01-07 20:07:23 +0800

    上传记录修改

--- src/modules/paper-upload-list/index.vue
+++ src/modules/paper-upload-list/index.vue
@@ -2,7 +2,7 @@
 
   <div class="panel panel-default">
     <div class="panel-body">
-
+      <search-view></search-view>
       <table-view></table-view>
       <edit-view></edit-view>
       <confirm :open.sync="state.table.removeConfirm.open" @confirm="on_item_remove" :saving.sync="state.table.removeConfirm.saving"></confirm>
@@ -15,13 +15,15 @@
   import { state, actions } from './store'
   import TableView from './table'
   import EditView from './edit'
+  import SearchView from './search'
 
   export default {
     name: 'PaperUploadList',
     extends: configBaseComponent({ state, actions }),
     components: {
       TableView,
-      EditView
+      EditView,
+      SearchView
     }
   }
 </script>
